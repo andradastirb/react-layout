7290dd90f4496b303fccaee5e03af064457cabb9
Andrada Stirb <andradaroxanastirb@gmail.com>
2023-11-22 18:20:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Template } from "./template/Template";
 
 function App() {
+  const myRouter = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Template />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h3>This page is not available!</h3>} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <p>Hello</p> */}
+      <RouterProvider router={myRouter} />
     </div>
   );
 }
